45dadcacd5bc4b0b2b07d499c572b552b0dc9e8a
Javier López Úbeda <jlopezcur@gmail.com>
2024-01-29 23:43:03 +0100

    style: less rounded cards

--- src/components/Card/StyledCard.ts
+++ src/components/Card/StyledCard.ts
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const StyledCard = styled.div`
   background: white;
   border: 1px solid #007ea7;
-  border-radius: 0.5rem;
+  border-radius: 0.3rem;
   margin-bottom: 1rem;
   overflow: hidden;
   box-shadow: 0.5rem 0.5rem 1rem rgba(0, 126, 167, 0.3);
