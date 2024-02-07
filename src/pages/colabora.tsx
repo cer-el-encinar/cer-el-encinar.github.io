cae6f25d0cf001de104de676fa3958e7f5cc73d2
Javier López Úbeda <jlopezcur@gmail.com>
2024-02-07 01:26:49 +0100

    feat: normalize colabora page

--- src/pages/colabora.tsx
+++ src/pages/colabora.tsx
@@ -39,12 +39,14 @@ const ColaboraPage: React.FC<PageProps> = () => (
         personas compasivas y comprometidas para asegurarse de que cada miembro
         de esta adorable pandilla tenga una vida plena y saludable.
       </p>
+
+      <h4>¿Te gustaría ser parte de esta misión maravillosa?</h4>
       <p>
-        ¿Te gustaría ser parte de esta misión maravillosa? Convertirte en
-        voluntario de nuestra asociación de cuidados de colonias felinas es una
-        experiencia gratificante que marca la diferencia en la vida de estos
-        encantadores mininos.
+        Convertirte en voluntario de nuestra asociación de cuidados de colonias
+        felinas es una experiencia gratificante que marca la diferencia en la
+        vida de estos encantadores mininos.
       </p>
+
       <p>
         Imagina ser el faro de esperanza para esos ojitos brillantes que miran
         con curiosidad y ternura. Tu tiempo y dedicación pueden marcar la
@@ -52,24 +54,29 @@ const ColaboraPage: React.FC<PageProps> = () => (
         cuidados. Al unirte a nosotros, te conviertes en el defensor de aquellos
         que no pueden hablar por sí mismos.
       </p>
+
+      <h4>¿Qué puedes hacer como voluntario?</h4>
       <p>
-        ¿Qué puedes hacer como voluntario? Las posibilidades son infinitas.
-        Desde alimentar a los gatos y proporcionar refugio, hasta participar en
-        campañas de esterilización y concientización en la comunidad. Si tienes
-        habilidades médicas o simplemente te encanta pasar tiempo jugando con
-        estos adorables amigos, hay un lugar para ti en nuestro equipo.
+        Las posibilidades son infinitas. Desde alimentar a los gatos y
+        proporcionar refugio, hasta participar en campañas de esterilización y
+        concientización en la comunidad. Si tienes habilidades médicas o
+        simplemente te encanta pasar tiempo jugando con estos adorables amigos,
+        hay un lugar para ti en nuestro equipo.
       </p>
+
       <p>
         La satisfacción de saber que estás contribuyendo a mejorar las vidas de
         estos gatos callejeros es incomparable. Cada acto de bondad, por pequeño
         que sea, suma un gran impacto en el bienestar de estas criaturas
         vulnerables.
       </p>
+
       <p>
         No necesitas ser un experto en gatos para unirte a nosotros; solo
         necesitas un corazón lleno de compasión y el deseo de marcar la
         diferencia. ¡Cada voluntario cuenta!
       </p>
+
       <p>
         Así que, ¿te unes a nosotros? Sé parte de esta familia de amantes de los
         felinos, donde cada ronroneo y cada cola levantada es una prueba del
