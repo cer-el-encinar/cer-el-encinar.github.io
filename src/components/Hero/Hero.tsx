9e0e391329708c6ec5f51f72d7cfbca41770b08e
Javier López Úbeda <jlopezcur@gmail.com>
2024-09-29 03:00:48 +0200

    fix: hero background image

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,5 +1,3 @@
-import { getImageProps } from 'next/image';
-
 import { Contained } from '../Contained';
 import { Subtitle, Title } from './components';
 import BgImage from './bg.png';
@@ -9,30 +7,14 @@ type Props = {
   small?: boolean;
 };
 
-function getBackgroundImage(srcSet = '') {
-  const imageSet = srcSet
-    .split(', ')
-    .map((str) => {
-      const [url, dpi] = str.split(' ');
-      return `url("${url}") ${dpi}`;
-    })
-    .join(', ');
-  return `image-set(${imageSet})`;
-}
-
 const $Hero: React.FC<Props> = ({ children, small = false }) => {
-  const {
-    props: { srcSet },
-  } = getImageProps({ alt: '', width: 500, height: 500, src: BgImage });
-  const bgImage = getBackgroundImage(srcSet);
   return (
     <div
       className={`text-4xl px-0 py-[2rem] ${
         small ? 'md:py-[4rem]' : 'md:py-[8rem]'
       }`}
       style={{
-        backgroundImage: bgImage,
-        backdropFilter: 'revert-layer',
+        backgroundImage: `url(${BgImage.src})`,
         backgroundSize: '500px',
       }}
     >
